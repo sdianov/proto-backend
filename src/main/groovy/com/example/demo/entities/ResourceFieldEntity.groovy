7e5c8c9dc78a96153b44358f86257b7d2c2f2718
S.Dianov <sdianov@griddynamics.com>
2019-12-25 13:42:38 +0400

    type fields

---
+++ src/main/groovy/com/example/demo/entities/ResourceFieldEntity.groovy
@@ -0,0 +1,29 @@
+package com.example.demo.entities
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.SequenceGenerator
+import javax.persistence.Table
+
+@Entity
+@Table(name = "resource_field")
+@SequenceGenerator(name = "seq_resource_field", initialValue = 1, allocationSize = 100)
+class ResourceFieldEntity {
+
+    @Id
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "name")
+    String name;
+
+    @ManyToOne
+    @JoinColumn(name = "resource_type_id")
+    @JsonIgnore
+    ResourceTypeEntity resourceType;
+}
--- src/main/groovy/com/example/demo/entities/ResourceTypeEntity.groovy
+++ src/main/groovy/com/example/demo/entities/ResourceTypeEntity.groovy
@@ -5,6 +5,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.OneToMany
 import javax.persistence.SequenceGenerator
 import javax.persistence.Table
 
@@ -20,4 +21,9 @@ class ResourceTypeEntity {
 
     @Column(name = "name")
     String name;
+
+    @OneToMany(mappedBy = "resourceType")
+    List<ResourceFieldEntity> fieldList;
+
+
 }
